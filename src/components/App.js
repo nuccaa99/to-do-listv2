1ed1a4dd47162bca3c4f763ee3ed86b5014d8314
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2023-07-06 23:36:43 +0400

    add task adding action

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import "../style.css"
 
 const App = () => {
+    const [task, setTask] = useState("")
+    const [taskList, setTaskList] = useState([]);
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setTaskList([...taskList, task])
+        setTask("")
+    }
+
+    const handleInputChange = (event) => {
+        if (event.target.value.length > 0) {
+            setTask(event.target.value);
+        }
+    }
+
+
     return (
         <div className="container">
             <div className="wrapper">
                 <header>To-Do.</header>
                 <div className="list--container">
-                    <div className="list"></div>
-                    <input type="text" placeholder="new task." className="list--input"></input>
-                    <button className="list--button">add task.</button>
+                    <div className="list">
+                        {taskList.map((task) => {
+                            return (
+                                <li key={taskList.indexOf(task)}>{task}</li>)
+                        })}
+                    </div>
+                    <input value={task} type="text" placeholder="new task." className="list--input" onChange={handleInputChange}></input>
+                    <button type="submit" className="list--button" onClick={handleClick}>add task.</button>
                 </div>
             </div>
         </div>
