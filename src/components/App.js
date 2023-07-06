84a681aa69b5006e1fefff53fb9ebe60f674b6e2
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2023-07-06 22:31:00 +0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style.css"
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="wrapper">
+                <header>To-Do</header>
+                <div className="list--container">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
